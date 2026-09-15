e714e7ba3c8f3dbed46db117ce51a5ebb079a722
Damião <damiao.bh@gmail.com>
2026-09-15 13:05:45 -0300

    Ajuste pipeline-5

--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,20 +1,25 @@
+# 1. Estas são as importações que estavam faltando!
+from guara.transaction import AbstractTransaction
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# 2. A sua classe agora vai reconhecer o AbstractTransaction
 class CheckoutTransaction(AbstractTransaction):
     
-    # CORREÇÃO: Adicionados os ** antes do kwargs e ajustado o driver
     def do(self, **kwargs):
-        # No framework Guara, o driver costuma vir de self.driver
         driver = self.driver 
         wait = WebDriverWait(driver, 10)
         
-        # Clicar no botão de Checkout que está na página do carrinho
+        # Clicar no botão de Checkout
         driver.find_element(By.ID, "checkout").click()
         
-        # Esperar a página do formulário carregar buscando o first-name
+        # Esperar a página carregar buscando o first-name
         first_name_field = wait.until(
             EC.presence_of_element_located((By.ID, "first-name"))
         )
         
-        # Preencher os dados usando as kwargs
+        # Preencher os dados
         first_name_field.send_keys(kwargs.get("name"))
         driver.find_element(By.ID, "last-name").send_keys(kwargs.get("last"))
         driver.find_element(By.ID, "postal-code").send_keys(kwargs.get("zip_code"))
