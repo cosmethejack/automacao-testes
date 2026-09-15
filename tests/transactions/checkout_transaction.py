d48133b8eefdea19dc13f3517b707bb8f604087a
Damião <damiao.bh@gmail.com>
2026-09-15 19:10:06 -0300

    Corrige CheckoutTransaction

--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,65 +1,39 @@
-def do(self, **kwargs):
-    driver = self._driver
-    wait = WebDriverWait(driver, 10)
+import time
+from guara.transaction import AbstractTransaction
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-    print("URL antes do checkout:", driver.current_url)
 
-    checkout_btn = wait.until(
-        EC.element_to_be_clickable((By.ID, "checkout"))
-    )
+class CheckoutTransaction(AbstractTransaction):
 
-    driver.execute_script(
-        "arguments[0].click();",
-        checkout_btn
-    )
+    def do(self, **kwargs):
+        driver = self._driver
+        wait = WebDriverWait(driver, 10)
 
-    time.sleep(2)
-
-    print("URL depois do checkout:", driver.current_url)
-
-    wait.until(
-        EC.presence_of_element_located(
-            (By.ID, "first-name")
+        checkout_btn = wait.until(
+            EC.element_to_be_clickable((By.ID, "checkout"))
         )
-    )
+        checkout_btn.click()
 
-    driver.find_element(
-        By.ID,
-        "first-name"
-    ).send_keys(kwargs.get("name"))
+        wait.until(
+            EC.presence_of_element_located((By.ID, "first-name"))
+        )
 
-    driver.find_element(
-        By.ID,
-        "last-name"
-    ).send_keys(kwargs.get("last"))
-
-    driver.find_element(
-        By.ID,
-        "postal-code"
-    ).send_keys(kwargs.get("zip_code"))
-
-    print(
-        "Primeiro nome:",
         driver.find_element(
-            By.ID,
-            "first-name"
-        ).get_attribute("value")
-    )
+            By.ID, "first-name"
+        ).send_keys(kwargs.get("name"))
 
-    continue_btn = wait.until(
-        EC.element_to_be_clickable((By.ID, "continue"))
-    )
+        driver.find_element(
+            By.ID, "last-name"
+        ).send_keys(kwargs.get("last"))
 
-    driver.execute_script(
-        "arguments[0].click();",
-        continue_btn
-    )
+        driver.find_element(
+            By.ID, "postal-code"
+        ).send_keys(kwargs.get("zip_code"))
 
-    time.sleep(2)
+        time.sleep(1)
 
-    print(
-        "URL depois do continue:",
-        driver.current_url
-    )
+        driver.find_element(By.ID, "continue").click()
 
-    return driver.current_url
+        return driver.current_url
