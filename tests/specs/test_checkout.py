9821ca309f9851ddd4e80eaad7baf9691e78c0ff
Damião <damiao.bh@gmail.com>
2026-09-15 13:53:15 -0300

    Ajuste pipeline-8

--- tests/specs/test_checkout.py
+++ tests/specs/test_checkout.py
@@ -1,6 +1,8 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from tests.fixtures.driver import driver # Mantenha seu import de fixture
 
 def test_compra_produto_com_sucesso(driver):
     wait = WebDriverWait(driver, 10)
@@ -17,12 +19,15 @@ def test_compra_produto_com_sucesso(driver):
     # 3. IR PARA CARRINHO E CHECKOUT
     driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
     
-    # CORREÇÃO: Em vez de checar a URL, esperamos o botão de checkout ficar clicável
+    # GARANTIA CONTRA O REACT: Espera a rota mudar e "respira" 1 segundo
+    wait.until(EC.url_contains("cart.html"))
+    time.sleep(1) 
+    
     botao_checkout = wait.until(EC.element_to_be_clickable((By.ID, "checkout")))
     botao_checkout.click()
 
     # 4. PREENCHER DADOS
-    # Esperamos o campo de nome ficar visível
+    wait.until(EC.url_contains("checkout-step-one.html"))
     campo_nome = wait.until(EC.visibility_of_element_located((By.ID, "first-name")))
     campo_nome.send_keys("Damião")
     
@@ -31,7 +36,7 @@ def test_compra_produto_com_sucesso(driver):
     driver.find_element(By.ID, "continue").click()
     
     # 5. FINALIZAR COMPRA
-    # Esperamos o botão finish ficar clicável
+    wait.until(EC.url_contains("checkout-step-two.html"))
     botao_finish = wait.until(EC.element_to_be_clickable((By.ID, "finish")))
     botao_finish.click()
 
--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,20 +1,23 @@
-# 1. Estas são as importações que estavam faltando!
+import time
 from guara.transaction import AbstractTransaction
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# 2. A sua classe agora vai reconhecer o AbstractTransaction
 class CheckoutTransaction(AbstractTransaction):
     
     def do(self, **kwargs):
-        driver = self._driver
+        driver = self._driver 
         wait = WebDriverWait(driver, 10)
         
-        driver.find_element(By.ID, "checkout").click()
+        # Garantia contra o React para o botão ganhar a interatividade
+        time.sleep(1)
         
         # Clicar no botão de Checkout
         driver.find_element(By.ID, "checkout").click()
+        
+        # Esperar a URL mudar
+        wait.until(EC.url_contains("checkout-step-one.html"))
         
         # Esperar a página carregar buscando o first-name
         first_name_field = wait.until(
