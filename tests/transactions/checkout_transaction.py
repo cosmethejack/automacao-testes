6e24dad89e41f6fce5a3cd87ec5e3660e33243a5
Damião <damiao.bh@gmail.com>
2026-09-15 16:34:34 -0300

    Ajutes finais

--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,5 +1,6 @@
 from guara.transaction import AbstractTransaction
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -9,22 +10,22 @@ class CheckoutTransaction(AbstractTransaction):
         driver = self._driver 
         wait = WebDriverWait(driver, 10)
         
-        botao_checkout = wait.until(EC.presence_of_element_located((By.ID, "checkout")))
-        driver.execute_script("arguments[0].click();", botao_checkout)
+        botao_checkout = wait.until(EC.element_to_be_clickable((By.ID, "checkout")))
+        botao_checkout.click()
         
         wait.until(EC.url_contains("checkout-step-one.html"))
         
         first_name_field = wait.until(
-            EC.presence_of_element_located((By.ID, "first-name"))
+            EC.visibility_of_element_located((By.ID, "first-name"))
         )
         first_name_field.send_keys(kwargs.get("name"))
         driver.find_element(By.ID, "last-name").send_keys(kwargs.get("last"))
-        driver.find_element(By.ID, "postal-code").send_keys(kwargs.get("zip_code"))
         
-        botao_continue = driver.find_element(By.ID, "continue")
-        driver.execute_script("arguments[0].click();", botao_continue)
+        # O PULO DO GATO: Simular a tecla ENTER no último campo
+        postal_code_field = driver.find_element(By.ID, "postal-code")
+        postal_code_field.send_keys(kwargs.get("zip_code"))
+        postal_code_field.send_keys(Keys.RETURN)
         
-        # A MÁGICA PARA O GUARA FUNCIONAR: Esperar a tela mudar antes de retornar
         wait.until(EC.url_contains("checkout-step-two.html"))
         
         return driver.current_url
