5bdeaa64cbf6c46d506137e211326c4a7e578ea5
Damião <damiao.bh@gmail.com>
2026-09-15 15:31:27 -0300

    Ajuste pipeline-12

--- tests/specs/test_checkout.py
+++ tests/specs/test_checkout.py
@@ -9,39 +9,33 @@ def test_compra_produto_com_sucesso(driver):
 
     # 1. LOGIN
     driver.get("https://www.saucedemo.com/")
-    
-    # Aguarda o campo de usuário carregar
     campo_usuario = wait.until(EC.visibility_of_element_located((By.ID, "user-name")))
     campo_usuario.send_keys("standard_user")
-    
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
 
     # 2. ADICIONAR PRODUTO
-    # Aguarda o botão da mochila carregar após o login
     botao_mochila = wait.until(EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack")))
-    botao_mochila.click()
+    driver.execute_script("arguments[0].click();", botao_mochila)
 
     # 3. IR PARA CARRINHO E CHECKOUT
     driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    
     wait.until(EC.url_contains("cart.html"))
-    time.sleep(1) 
     
-    botao_checkout = wait.until(EC.element_to_be_clickable((By.ID, "checkout")))
-    botao_checkout.click()
+    # OPÇÃO NUCLEAR: Clique direto via JS no botão de checkout
+    botao_checkout = wait.until(EC.presence_of_element_located((By.ID, "checkout")))
+    driver.execute_script("arguments[0].click();", botao_checkout)
 
     # 4. PREENCHER DADOS
     campo_nome = wait.until(EC.visibility_of_element_located((By.ID, "first-name")))
     campo_nome.send_keys("Damião")
-    
     driver.find_element(By.ID, "last-name").send_keys("Barbosa")
     driver.find_element(By.ID, "postal-code").send_keys("30642-290")
     driver.find_element(By.ID, "continue").click()
     
     # 5. FINALIZAR COMPRA
-    botao_finish = wait.until(EC.element_to_be_clickable((By.ID, "finish")))
-    botao_finish.click()
+    botao_finish = wait.until(EC.presence_of_element_located((By.ID, "finish")))
+    driver.execute_script("arguments[0].click();", botao_finish)
 
     # 6. VALIDAÇÃO FINAL
     mensagem = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "complete-header")))
--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,4 +1,3 @@
-import time
 from guara.transaction import AbstractTransaction
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,25 +9,22 @@ class CheckoutTransaction(AbstractTransaction):
         driver = self._driver 
         wait = WebDriverWait(driver, 10)
         
-        # Garantia contra o React para o botão ganhar a interatividade
-        time.sleep(1)
+        # OPÇÃO NUCLEAR: Clique via JS para garantir que avança de tela
+        botao_checkout = wait.until(EC.presence_of_element_located((By.ID, "checkout")))
+        driver.execute_script("arguments[0].click();", botao_checkout)
         
-        # Clicar no botão de Checkout
-        driver.find_element(By.ID, "checkout").click()
-        
-        # Esperar a URL mudar
         wait.until(EC.url_contains("checkout-step-one.html"))
         
-        # Esperar a página carregar buscando o first-name
+        # Preencher os dados
         first_name_field = wait.until(
             EC.presence_of_element_located((By.ID, "first-name"))
         )
-        
-        # Preencher os dados
         first_name_field.send_keys(kwargs.get("name"))
         driver.find_element(By.ID, "last-name").send_keys(kwargs.get("last"))
         driver.find_element(By.ID, "postal-code").send_keys(kwargs.get("zip_code"))
         
-        driver.find_element(By.ID, "continue").click()
+        # Clicar em continuar
+        botao_continue = driver.find_element(By.ID, "continue")
+        driver.execute_script("arguments[0].click();", botao_continue)
         
         return driver.current_url
