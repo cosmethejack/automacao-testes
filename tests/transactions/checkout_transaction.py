8126391989edff0a1248482006a42203ead924b5
Damião <damiao.bh@gmail.com>
2026-09-15 19:03:27 -0300

    novos ajustes utilizando copilot4

--- tests/transactions/checkout_transaction.py
+++ tests/transactions/checkout_transaction.py
@@ -1,34 +1,65 @@
-import time
-from guara.transaction import AbstractTransaction
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+def do(self, **kwargs):
+    driver = self._driver
+    wait = WebDriverWait(driver, 10)
 
-class CheckoutTransaction(AbstractTransaction):
-    
-    def do(self, **kwargs):
-        driver = self._driver 
-        wait = WebDriverWait(driver, 10)
-        
-        botao_checkout = wait.until(EC.presence_of_element_located((By.ID, "checkout")))
-        driver.execute_script("arguments[0].click();", botao_checkout)
-        
-        wait.until(EC.url_contains("checkout-step-one.html"))
-        
-        first_name_field = wait.until(
-            EC.visibility_of_element_located((By.ID, "first-name"))
+    print("URL antes do checkout:", driver.current_url)
+
+    checkout_btn = wait.until(
+        EC.element_to_be_clickable((By.ID, "checkout"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        checkout_btn
+    )
+
+    time.sleep(2)
+
+    print("URL depois do checkout:", driver.current_url)
+
+    wait.until(
+        EC.presence_of_element_located(
+            (By.ID, "first-name")
         )
-        first_name_field.send_keys(kwargs.get("name"))
-        driver.find_element(By.ID, "last-name").send_keys(kwargs.get("last"))
-        driver.find_element(By.ID, "postal-code").send_keys(kwargs.get("zip_code"))
-        
-        # A SOLUÇÃO DEFINITIVA: 1 segundo para o React registrar os textos
-        time.sleep(1)
-        
-        # Clique normal
-        botao_continue = wait.until(EC.element_to_be_clickable((By.ID, "continue")))
-        botao_continue.click()
-        
-        wait.until(EC.url_contains("checkout-step-two.html"))
-        
-        return driver.current_url
+    )
+
+    driver.find_element(
+        By.ID,
+        "first-name"
+    ).send_keys(kwargs.get("name"))
+
+    driver.find_element(
+        By.ID,
+        "last-name"
+    ).send_keys(kwargs.get("last"))
+
+    driver.find_element(
+        By.ID,
+        "postal-code"
+    ).send_keys(kwargs.get("zip_code"))
+
+    print(
+        "Primeiro nome:",
+        driver.find_element(
+            By.ID,
+            "first-name"
+        ).get_attribute("value")
+    )
+
+    continue_btn = wait.until(
+        EC.element_to_be_clickable((By.ID, "continue"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        continue_btn
+    )
+
+    time.sleep(2)
+
+    print(
+        "URL depois do continue:",
+        driver.current_url
+    )
+
+    return driver.current_url
