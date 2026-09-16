644661e7aa315ef3193459e5fa0a47cb0b58401e
Damião <damiao.bh@gmail.com>
2026-09-15 21:48:08 -0300

    fix: padroniza test_login_2 usando Page Objects com clique robusto

--- tests/specs/test_login_2.py
+++ tests/specs/test_login_2.py
@@ -1,27 +1,25 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from tests.pages.login_page import LoginPage
+from tests.pages.inventory_page import InventoryPage
+from tests.pages.cart_page import CartPage
 
 
 def test_continuar_comprando_apos_adicionar_item(driver):
-    wait = WebDriverWait(driver, 10)
+    login_page = LoginPage(driver)
+    inventory_page = InventoryPage(driver)
+    cart_page = CartPage(driver)
 
     # 1. LOGIN
-    driver.get("https://www.saucedemo.com/")
-    wait.until(EC.element_to_be_clickable((By.ID, "user-name"))).send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
+    login_page.open("https://www.saucedemo.com/")
+    login_page.login("standard_user", "secret_sauce")
+    assert inventory_page.is_loaded()
 
-    # 2. ADICIONAR ITEM AO CARRINHO
-    wait.until(EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))).click()
+    # 2. ADICIONAR ITEM AO CARRINHO E IR AO CARRINHO
+    inventory_page.add_product()
+    inventory_page.go_to_cart()
 
-    # 3. IR AO CARRINHO
-    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    wait.until(EC.url_contains("cart.html"))
+    # 3. CLICAR EM CONTINUAR COMPRANDO
+    cart_page.continue_shopping()
 
-    # 4. CLICAR EM CONTINUAR COMPRANDO
-    wait.until(EC.element_to_be_clickable((By.ID, "continue-shopping"))).click()
-
-    # 5. VALIDAR RETORNO À TELA DE INVENTÁRIO
-    wait.until(EC.url_contains("inventory.html"))
+    # 4. VALIDAR RETORNO À TELA DE INVENTÁRIO
+    assert inventory_page.is_loaded()
     assert "inventory.html" in driver.current_url
